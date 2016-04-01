58e204a241c42d3d1db5037e540b35b54cd34fd5
jennz0r <jenn.ew@gmail.com>
2016-04-01 17:57:29 -0400

    add and remove images and new folders

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function scrollDown(name, time) {
+  var aTag = $("." + name);
+  $('html,body').animate({
+      scrollTop: aTag.offset().top - 75
+    },
+    time, 'swing');
+}
+
+$('.aboutus').click(function() {
+  scrollDown('about-us', 1000);
+});
+
+$('.howitworks').click(function() {
+  scrollDown('how-it-works', 1000);
+});
+
+$('.why').click(function() {
+  scrollDown('why-why', 1000);
+});
+
+$('.whatwedo').click(function() {
+  scrollDown('what-we-do', 1000);
+});
+
