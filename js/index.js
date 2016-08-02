f39937cf67443f4b5cc52250cd01a2ed0a09cd96
Michelle Glauser <michelleglauser@gmail.com>
2016-08-01 18:56:27 -0700

    Remove openPopupForm from document ready function.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+function openPopupForm() {
+  document.cookie = 'MCEvilPopupClosed=;path=/;expires=Thu, 01 Jan 1970 00:00:00 UTC;';
+  require(["mojo/signup-forms/Loader"],
+  function (L) {
+    L.start({"baseUrl":"mc.us13.list-manage.com","uuid":"110f637e29d9b2b5f89664fe8","lid":"22f1ab3b1f"})
+  });
+}
+
 $(document).ready(function() {
 
   function scrollDown(name, time) {
@@ -6,14 +14,6 @@ $(document).ready(function() {
         scrollTop: aTag.offset().top - 75
       },
       time, 'swing');
-  }
-
-  function openPopupForm() {
-    document.cookie = 'MCEvilPopupClosed=;path=/;expires=Thu, 01 Jan 1970 00:00:00 UTC;';
-    require(["mojo/signup-forms/Loader"],
-    function (L) {
-      L.start({"baseUrl":"mc.us13.list-manage.com","uuid":"110f637e29d9b2b5f89664fe8","lid":"22f1ab3b1f"})
-    });
   }
 
   $('.howitworks').click(function() {
