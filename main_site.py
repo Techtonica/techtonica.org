90a020d9ccbe18021a5a6c6d1e8570036ca32131
Harry Staley <staleyh@gmail.com>
2016-08-05 21:55:08 -0500

    FEAT: Started Main.py
    Created a basic template framework for use with Flask

---
+++ main_site.py
@@ -0,0 +1,33 @@
+"""
+This is the main python file that sets up rendering and templating
+for Techtonica.com
+"""
+from flask import Flask, render_template
+
+__author__ = "Harry Staley <staleyh@gmail.com>"
+__version__ = "1.0"
+
+app = Flask(__name__)
+
+
+# MAIN HANDLERS
+@app.route('/')
+def render_home_page():
+    '''
+    Renders the home page from templates
+    '''
+    return render_template('index.html')
+
+
+@app.route('/about/')
+def render_about_page():
+    '''
+    Renders the about page from templates
+    '''
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
