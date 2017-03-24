a7e816bdcebdf4d86b1912145852db996bc956bf
Michelle Glauser <michelleglauser@gmail.com>
2017-03-24 07:07:26 -0700

    Separate scrolling JavaScript from index.js, place scripts in Jinja blocks, override in thankyou.html.

--- static/js/index.js
+++ static/js/index.js
@@ -49,38 +49,4 @@ $(document).ready(function() {
     openPopupForm();
     setCookie('techtonica-visited', 'yes', 9999)
   }
-
-  function scrollDown(name, time) {
-    var aTag = $('.' + name);
-    $('html,body').animate({
-        scrollTop: aTag.offset().top - 75
-      },
-      time, 'swing');
-  }
-
-  $('.howitworks').click(function() {
-    scrollDown('how-it-works', 1000);
-  });
-
-  $('.why').click(function() {
-    scrollDown('why-why', 1000);
-  });
-
-  $('.whatwedo').click(function() {
-    scrollDown('what-we-do', 1000);
-  });
-
-  $('.howtohelp').click(function() {
-    scrollDown('how-to-help', 1000);
-  });
-
-  $('.supporters').click(function() {
-    scrollDown('our-supporters', 1000)
-  });
-
-  $('.lines-button').click(function(){
-    $(this).toggleClass('x close');
-    $('.nav-bar').toggleClass('small-screen-nav');
-    $('.nav-link').toggleClass('nav-link--mobile');
-  });
 });
