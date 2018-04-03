5d103e664bc08b9431670386951bf6435182854f
MichelleGlauser <michelleglauser@gmail.com>
2018-04-03 11:44:00 -0700

    Change JS fix email function to attach to badmail class.

--- static/js/index.js
+++ static/js/index.js
@@ -37,7 +37,7 @@ function cookieExists(cname) {
   }
 }
 
-$('a.contact').on('click', function(){
+$('a.badmail').on('click', function(){
   var href = $(this).attr('href');
   $(this).attr('href', href.replace('badmail.', ''));
 });
