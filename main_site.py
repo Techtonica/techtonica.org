1fca18c2d9004771be9f64269fce229b20d5963b
Michelle Glauser <michelleglauser@gmail.com>
2020-10-05 14:46:36 -0700

    Uncomment SEAM route and link on openings page.

--- main_site.py
+++ main_site.py
@@ -100,12 +100,10 @@ def render_openings_page():
     return render_template("openings.html")
 
 
-"""
 @app.route("/openings/seam/")
 def render_seam_page():
     Renders the openings page from jinja2 template
     return render_template("seam.html")
-"""
 
 
 @app.route("/openings/curriculumdev/")
