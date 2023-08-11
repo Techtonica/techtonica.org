d84306666dd55a6009886a9a24f6a56701268bab
Michelle Glauser <MichelleGlauser@users.noreply.github.com>
2023-08-10 18:13:08 -0700

    Comment out STEM JD route

--- main_site.py
+++ main_site.py
@@ -100,12 +100,12 @@ def render_openings_page():
     return render_template("openings.html")
 
 
-@app.route("/openings/stem/")
-def render_stem_page():
-    """
-    Renders the STEM page from jinja2 template
-    """
-    return render_template("stem.html")
+# @app.route("/openings/stem/")
+# def render_stem_page():
+#     """
+#     Renders the STEM page from jinja2 template
+#     """
+#     return render_template("stem.html")
 
 
 @app.route("/openings/tapm/")
