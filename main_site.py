6308d849c7da9006cac73f56e898a9149868d229
Monikka de la Zerda <monikka.delazerda@gmail.com>
2025-01-07 10:00:02 -1000

    test changing route

--- main_site.py
+++ main_site.py
@@ -98,7 +98,7 @@ def render_faqs_page():
     return render_template("faqs.html")
 
 
-@app.route("/openings/")
+@app.route("/openings_page/")
 def render_openings_page():
     """
     Renders the openings page from jinja2 template
