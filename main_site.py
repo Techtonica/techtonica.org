2710fcedbb46d975d45d26d13c211999f942fd8a
Thane Chaves <tmoonder@gmail.com>
2025-02-06 15:56:53 -0500

    Corrected indentation issue that was causing app to crash when running get_time().

--- main_site.py
+++ main_site.py
@@ -264,7 +264,8 @@ def get_time():
         app_close_date = app_open_date + datetime.timedelta(days=28)
         date_string = app_close_date.strftime("%B %-d")
         text = "Apply by {date} (12pm PT)!".format(date=date_string)
-        app_open = app_open_date <= today <= app_close_date
+
+    app_open = app_open_date <= today <= app_close_date
 
     return {
         "app_open": app_open,
