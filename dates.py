bb28453fcdaf23abfb48445113a6f2357a9fd6ec
Eduardo Vazquez <80133388+vazquezea96@users.noreply.github.com>
2025-10-21 15:52:48 -0700

    Created new branch to fix css issues from last branch, font color updated and button size updated.

--- dates.py
+++ dates.py
@@ -85,7 +85,7 @@ def generate_application_timeline():
 
     # Determine application status
     app_open = app_open_datetime and (app_open_datetime <= today <= app_close_datetime)
-    
+
     # Flag to determine if applications are closed (past)
     # app_closed = app_open_datetime and (today > app_close_datetime)
 
@@ -135,27 +135,46 @@ def generate_application_timeline():
         cohort_start_day + timedelta(weeks=48) if cohort_start_day else None
     )
 
-    # Text logic - only show application-specific text when applications are currently open
+    # Text logic - show app-specific text when applications are open
     if app_open and app_close_datetime:
         # Applications are currently open
         main_text = f"Apply by {app_close_datetime.strftime('%B %d')} (12pm PT)!"
         fulltime_top_button = "Apply Now!"
-        fulltime_bottom_button = f"Apply by {app_close_datetime.strftime('%B %d')} (12pm PT)!"
-        
+        fulltime_bottom_button = (
+            f"Apply by {app_close_datetime.strftime('%B %d')} (12pm PT)!"
+        )
+
         # URLs for buttons when applications are open
-        fulltime_top_button_url = "https://docs.google.com/forms/d/e/1FAIpQLSdk8nJUSuK_xoILyYyf3GIpVypQRtqsx9aQE7odHgX1cWvoHA/viewform"
-        fulltime_bottom_button_url = "https://docs.google.com/forms/d/e/1FAIpQLSdk8nJUSuK_xoILyYyf3GIpVypQRtqsx9aQE7odHgX1cWvoHA/viewform"
-        home_button_url = "render_swe_program_page"  # This will be used with url_for in the template
+        fulltime_top_button_url = (
+            "https://docs.google.com/forms/d/e/"
+            "1FAIpQLSdk8nJUSuK_xoILyYyf3GIpVypQRtqsx9aQE7odHgX1cWvoHA/viewform"
+        )
+        fulltime_bottom_button_url = (
+            "https://docs.google.com/forms/d/e/"
+            "1FAIpQLSdk8nJUSuK_xoILyYyf3GIpVypQRtqsx9aQE7odHgX1cWvoHA/viewform"
+        )
+        home_button_url = (
+            "render_swe_program_page"  # This will be used with url_for in the
+            # template
+        )
     else:
         # Applications are closed, not scheduled, or scheduled for the future
         main_text = "Outsource your software work!"
         fulltime_top_button = "Get notified about the next cohort!"
         fulltime_bottom_button = "Sign up to join our events"
-        
+
         # URLs for buttons when applications are closed
-        fulltime_top_button_url = "https://docs.google.com/forms/d/e/1FAIpQLSfUdyIAfcU5KSqtYH5J5iPRgu-yycHdebnUKygQLEv-m7oVMw/viewform"
-        fulltime_bottom_button_url = "https://www.eventbrite.com/o/techtonica-11297022451" 
-        home_button_url = "render_consulting_page"  # This will be used with url_for in the template
+        fulltime_top_button_url = (
+            "https://docs.google.com/forms/d/e/"
+            "1FAIpQLSfUdyIAfcU5KSqtYH5J5iPRgu-yycHdebnUKygQLEv-m7oVMw/viewform"
+        )
+        fulltime_bottom_button_url = (
+            "https://www.eventbrite.com/o/techtonica-11297022451"
+        )
+        home_button_url = (
+            "render_consulting_page"  # This will be used with url_for in the
+            # template
+        )
 
     return {
         "APP_OPEN_DATE": format_date(app_open_datetime),
@@ -178,11 +197,12 @@ def generate_application_timeline():
         "JOB_SEARCH_START_MONTH_YEAR": format_month_year(training_end),
         "JOB_SEARCH_END_MONTH_YEAR": format_month_year(job_search_end),
         "APP_OPEN": app_open,
-        # "APP_CLOSED": app_closed,  # not being used but present for potential future use
+        # "APP_CLOSED": app_closed,  # Not used, but kept for potential future
+        # use
         "TEXT": main_text,
         "FULLTIME_TOP_BUTTON": fulltime_top_button,
         "FULLTIME_BOTTOM_BUTTON": fulltime_bottom_button,
         "FULLTIME_TOP_BUTTON_URL": fulltime_top_button_url,
         "FULLTIME_BOTTOM_BUTTON_URL": fulltime_bottom_button_url,
-        "HOME_BUTTON_URL": home_button_url
+        "HOME_BUTTON_URL": home_button_url,
     }
